e8d3b31ac9176e9bb62dd855f1b6434938495ce5
dddmogui13 <dmytropotorocha213@gmail.com>
2023-10-08 13:15:09 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+function getInputValues() {
+  const {
+    elements: { delay, step, amount },
+  } = form;
+  return {
+    delay: Number(delay.value),
+    step: Number(step.value),
+    amount: Number(amount.value),
+  };
+}
+
+async function formSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = getInputValues();
+
+  for (let i = 0; i < amount; i++) {
+    try {
+      await create(i + 1, delay);
+      Notify.success(`✅ Fulfilled promise ${i + 1} in ${delay}ms`);
+    } catch (error) {
+      Notify.failure(`❌ Rejected promise ${i + 1} in ${delay}ms`);
+    }
+    delay += step;
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+async function create(position, delay) {
+  return createPromise(position, delay);
+}
+
+form.addEventListener('submit', formSubmit);
+form.addEventListener('input', getInputValues);

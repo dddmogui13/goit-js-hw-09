ee8b8d18f1076cb14056af34de0c55cee621e12a
dddmogui13 <133026210+dddmogui13@users.noreply.github.com>
2023-10-08 10:18:38 +0000

    Deploying to gh-pages from @ dddmogui13/goit-js-hw-09@c50c532952d3d630d7d7bee3df42583749e7af85 🚀

---
+++ 01-color-switcher.e687e804.js
@@ -0,0 +1,2 @@
+let e;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.e687e804.js.map
